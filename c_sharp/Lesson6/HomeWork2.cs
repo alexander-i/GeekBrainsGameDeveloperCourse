6b4a3e598b17a535f03cee57ad0f72a2c651f509
Alexander Ivanov <al.m.ivanov@gmail.com>
2021-02-13 02:22:46 +0300

    Lesson 6

---
+++ c_sharp/Lesson6/HomeWork2.cs
@@ -0,0 +1,91 @@
+﻿//
+// Александр Иванов (https://geekbrains.ru/users/4631092)
+//
+// 2. Модифицировать программу нахождения минимума функции так, чтобы можно было передавать функцию в виде делегата.
+
+// а) Сделайте меню с различными функциями и предоставьте пользователю выбор, для какой функции и на каком отрезке находить минимум.
+// б) Используйте массив(или список) делегатов, в котором хранятся различные функции.
+// в) *Переделайте функцию Load, чтобы она возвращала массив считанных значений. Пусть она
+// возвращает минимум через параметр.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lesson6
+{
+    public delegate double F(double x);
+    class HomeWork2
+    {
+
+        public static void SaveFunc(string fileName, F fun, double a, double b, double h)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            double x = a;
+            while (x <= b)
+            {
+                bw.Write(fun(x));
+                x += h;
+            }
+            bw.Close();
+            fs.Close();
+        }
+        public static double[] Load(string fileName, out double min_value)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader bw = new BinaryReader(fs);
+            double[] values = new double[fs.Length / sizeof(double)];
+            min_value = double.MaxValue;
+            double d;
+            for (int i = 0; i < fs.Length / sizeof(double); i++)
+            {
+                // Считываем значение и переходим к следующему
+                d = bw.ReadDouble();
+                values[i] = d;
+                if (d < min_value) min_value = d;
+            }
+            bw.Close();
+            fs.Close();
+            return values;
+        }
+        public static void Demo()
+        {
+            Dictionary<string, F> delegateDict = new Dictionary<string, F>();
+            delegateDict.Add("x * x - 50 * x + 10", delegate (double x) { return x * x - 50 * x + 10; });
+            delegateDict.Add("x * x * x - 10 + 5", delegate (double x) { return x * x * x - 10 + 5; });
+            delegateDict.Add("Math.Log(x * x) + 12", delegate (double x) { return Math.Log(x * x + 1) + 12; });
+            delegateDict.Add("Math.Log(x * x * 2) + 12", delegate (double x) { return Math.Log(x * x * 2 + 1) + 12; });
+            delegateDict.Add("Math.Exp(3 * x)", delegate (double x) { return Math.Exp(3 * x); });
+
+            // костыль для получения номера элемента далее в диалоге
+            string[] delegateKeysArray = delegateDict.Keys.ToArray<string>();
+
+            Utilities.Print("Выберите функцию для нахождения минимума:");
+            for (int i = 0; i < delegateKeysArray.Length; i++)
+            {
+                Utilities.Print($"{i}) {delegateKeysArray[i]}");
+            }
+
+            int selection = -1;
+            while (selection < 0 || selection > delegateKeysArray.Length - 1)
+            {
+                selection = Utilities.PrintAndReadAsInt32("Введите индекс функции: ", newlineAfterMsg: false);
+            }
+
+            int a = Utilities.PrintAndReadAsInt32("Введите начальное значение x: ", newlineAfterMsg: false);
+            int b = Utilities.PrintAndReadAsInt32("Введите конечное значение x: ", newlineAfterMsg: false);
+            double step = Utilities.PrintAndReadAsDouble("Введите шаг: ", newlineAfterMsg: false);
+
+            SaveFunc("data.bin", delegateDict[delegateKeysArray[selection]], a, b, step);
+            double[] data = Load("data.bin", out double min_value);
+            Utilities.Print($"Минимум: {min_value}");
+            Utilities.Print($"Данные:");
+            Utilities.Print(String.Join(" ", data));
+        }
+    }
+
+}
---
+++ c_sharp/Lesson6/HomeWork3.cs
@@ -0,0 +1,89 @@
+﻿//
+// Александр Иванов (https://geekbrains.ru/users/4631092)
+//
+// 3. Переделать программу «Пример использования коллекций» для решения следующих задач:
+
+// а) Подсчитать количество студентов учащихся на 5 и 6 курсах;
+// б) подсчитать сколько студентов в возрасте от 18 до 20 лет на каком курсе учатся (частотный массив);
+// в) отсортировать список по возрасту студента;
+// г) *отсортировать список по курсу и возрасту студента;
+// д) разработать единый метод подсчета количества студентов по различным параметрам
+// выбора с помощью делегата и методов предикатов.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Lesson6
+{
+    class Student
+    {
+        public string lastName;
+        public string firstName;
+        public string university;
+        public string faculty;
+        public int course;
+        public string department;
+        public int group;
+        public string city;
+        int age;
+        // Создаем конструктор
+        public Student(string firstName, string lastName, string university, string faculty, string department, int course, int age, int group, string city)
+        {
+            this.lastName = lastName;
+            this.firstName = firstName;
+            this.university = university;
+            this.faculty = faculty;
+            this.department = department;
+            this.course = course;
+            this.age = age;
+            this.group = group;
+            this.city = city;
+        }
+    }
+
+    class HomeWork3
+    {
+        static int MyDelegate(Student st1, Student st2)
+        {
+            return String.Compare(st1.firstName, st2.firstName);
+        }
+
+        public static void Demo()
+        {
+            int bakalavr = 0;
+            int magistr = 0;
+            List<Student> list = new List<Student>();
+            DateTime dt = DateTime.Now;
+            StreamReader sr = new StreamReader("../../students.csv");
+            while (!sr.EndOfStream)
+            {
+                try
+                {
+                    string[] s = sr.ReadLine().Split(';');
+                    list.Add(new Student(s[0], s[1], s[2], s[3], s[4], int.Parse(s[5]), int.Parse(s[6]), int.Parse(s[7]), s[8]));
+                    if (int.Parse(s[5]) < 5) bakalavr++; else magistr++;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine("Ошибка!ESC - прекратить выполнение программы");
+                    // Выход из Main
+                    if (Console.ReadKey().Key == ConsoleKey.Escape) return;
+                }
+            }
+            sr.Close();
+            list.Sort(new Comparison<Student>(MyDelegate));
+            Console.WriteLine("Всего студентов:" + list.Count);
+            Console.WriteLine("Магистров:{0}", magistr);
+            Console.WriteLine("Бакалавров:{0}", bakalavr);
+            foreach (var v in list) Console.WriteLine(v.firstName);
+            Console.WriteLine(DateTime.Now - dt);
+
+        }
+    }
+}
